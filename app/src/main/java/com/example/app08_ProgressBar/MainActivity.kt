d9f9be1486e80b6b3a79d897df5343a43eddb43c
srivas-ITB <santiago.rivas@itb.cat>
2026-03-02 12:58:01 +0100

    Inicio proves amb ProgressBar

---
+++ app/src/main/java/com/example/app08_ProgressBar/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.app08_ProgressBar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.app08_ProgressBar.ui.theme.MyApp_Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApp_Theme() {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MyExample(modifier = Modifier.padding( innerPadding))
+                }
+            }
+        }
+
+    }
+}
+
+
+@Composable
+fun MyExample(modifier: Modifier = Modifier) {
+
+    Column(
+        Modifier.fillMaxSize().verticalScroll( rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+        Spacer(modifier = Modifier.padding(10.dp))
+        MyProgressBar1()
+
+    }
+}
+
+@Composable
+fun MyProgressBar1(){
+    Text("Progress Bar circular")
+    CircularProgressIndicator()
+}
